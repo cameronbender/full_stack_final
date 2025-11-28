b204e21e13497640009fe7afaac25abb77614f21
isaaccampbell <isaac@Judique.ca>
2025-11-28 19:54:26 -0400

    submission.html changes and moved some files into public directory

--- CarbonRoots/server.js
+++ CarbonRoots/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // Database Connection
 if (!process.env.DATABASE_URL || process.env.DATABASE_URL === 'postgresql://user:password@host:port/database') {
